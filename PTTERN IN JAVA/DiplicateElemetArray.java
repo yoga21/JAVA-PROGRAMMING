2e7260ec316407abf96c22aa213c921a1bdbe722
yoga21 <50547906+yoga21@users.noreply.github.com>
2020-04-22 10:30:54 +0530

    Add files via upload

---
+++ PTTERN IN JAVA/DiplicateElemetArray.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class DiplicateElemetArray {
+	
+	public static void main(String args[]) {
+		
+		System.out.println("Enter the number");
+		
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		
+		System.out.println(" the number"+n);
+		pyramidPattern(n);
+	}
+
+	public static void pyramidPattern(int n) {
+		
+		for(int i=0;i<n;i++) {
+			
+			 
+			for(int j=n;j>=i+1;j--) {
+				 System.out.print("* ");
+				
+			}
+			
+			
+			for(int j=0;j<=i;j++) {
+				 System.out.print("  ");
+				
+			}
+			for(int j=n;j>=i+1;j--) {
+				 System.out.print("* ");
+				
+			}
+			for(int j=0;j<=i;j++) {
+				 System.out.print("  ");
+				
+			}
+			for(int j=n;j>=i+1;j--) {
+				 System.out.print("* ");
+				
+			}
+			for(int j=0;j<=i;j++) {
+				 System.out.print("  ");
+				
+			}
+			for(int j=n;j>=i+1;j--) {
+				 System.out.print("* ");
+				
+			}
+			for(int j=0;j<=i;j++) {
+				 System.out.print("  ");
+				
+			}
+			for(int j=n;j>=i+1;j--) {
+				 System.out.print("* ");
+				
+			}
+			for(int j=0;j<=i;j++) {
+				 System.out.print("  ");
+				
+			}
+			for(int j=n;j>=i+1;j--) {
+				 System.out.print("* ");
+				
+			}
+			for(int j=0;j<=i;j++) {
+				 System.out.print("  ");
+				
+			}
+			 System.out.println();
+		}
+		
+		
+	}
+
+}
